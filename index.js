f9dab16464de72b68ab52acd6f83076253da30fc
Pooja Kodavanti <pkodavan@gmail.com>
2017-12-16 06:15:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
-  var name = "you";
   return "Happy holidays, ${name}!"
 }
 function happyHolidayTo(holiday, name) {
